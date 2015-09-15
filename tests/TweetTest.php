2dc4ea86f9f1b25a6552b3358c3cddd0758cef8c
James Lind <james@officesupply.com>
2015-09-15 17:20:29 -0500

    Update Tweet tests now that the Tweet is immutable.

--- tests/TweetTest.php
+++ tests/TweetTest.php
@@ -3,8 +3,12 @@
 namespace JimLind\Pie7o\Tests;
 
 use JimLind\Pie7o\Tweet;
+use PHPUnit_Framework_TestCase;
 
-class TweetTest extends \PHPUnit_Framework_TestCase
+/**
+ * Test the JimLind\Pie7o\Tweet class
+ */
+class TweetTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @var Tweet
@@ -16,6 +20,9 @@ class TweetTest extends \PHPUnit_Framework_TestCase
         $this->fixture = new Tweet();
     }
 
+    /**
+     * Test an empty Tweet has not data
+     */
     public function testEmptyTweet()
     {
         $this->assertNull($this->fixture->getMessage());
@@ -23,37 +30,60 @@ class TweetTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(0, $this->fixture->getMediaId());
     }
 
+    /**
+     * Test giving a Tweet a message stream allows you to get it
+     */
     public function testMessageSetGet()
     {
         $stream = $this->getMock('Psr\Http\Message\StreamInterface');
-        $this->fixture->setMessage($stream);
-        $this->assertSame($stream, $this->fixture->getMessage());
+        $output = $this->fixture->withMessage($stream);
+
+        $this->assertSame($stream, $output->getMessage());
 
         // Make sure other properties are not affected
+        $this->assertNull($output->getMedia());
+        $this->assertSame(0, $output->getMediaId());
+
+        $this->assertNull($this->fixture->getMessage());
         $this->assertNull($this->fixture->getMedia());
         $this->assertSame(0, $this->fixture->getMediaId());
     }
 
+    /**
+     * Test giving a Tweet a media stream allows you to get it
+     */
     public function testMediaSetGet()
     {
         $stream = $this->getMock('Psr\Http\Message\StreamInterface');
-        $this->fixture->setMedia($stream);
-        $this->assertSame($stream, $this->fixture->getMedia());
+        $output = $this->fixture->withMedia($stream);
+
+        $this->assertSame($stream, $output->getMedia());
 
         // Make sure other properties are not affected
+        $this->assertNull($output->getMessage());
+        $this->assertSame(0, $output->getMediaId());
+
+        $this->assertNull($this->fixture->getMedia());
         $this->assertNull($this->fixture->getMessage());
         $this->assertSame(0, $this->fixture->getMediaId());
     }
 
+    /**
+     * Test giving a Tweet a media id allows you to get it
+     */
     public function testMediaIdSetGet()
     {
         $mediaId     = rand();
         $mediaString = (string) $mediaId;
+        $output      = $this->fixture->withMediaId($mediaString);
 
-        $this->fixture->setMediaId($mediaString);
-        $this->assertSame($mediaId, $this->fixture->getMediaId());
+        $this->assertSame($mediaId, $output->getMediaId());
 
         // Make sure other properties are not affected
+        $this->assertNull($output->getMedia());
+        $this->assertNull($output->getMessage());
+
+        $this->assertSame(0, $this->fixture->getMediaId());
         $this->assertNull($this->fixture->getMessage());
         $this->assertNull($this->fixture->getMedia());
     }
