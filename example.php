43d8f6f454101314df96c79104d86aee95d6fbbb
James Lind <james@officesupply.com>
2015-09-15 17:01:00 -0500

    Don't use full namespaces because nobody will in real life.

--- example.php
+++ example.php
@@ -1,6 +1,14 @@
 <?php
 require 'vendor/autoload.php';
 
+use GuzzleHttp\Psr7\Stream;
+use JimLind\Pie7o\AuthorizationBuilder;
+use JimLind\Pie7o\MediaUploader;
+use JimLind\Pie7o\Pie7oException;
+use JimLind\Pie7o\StatusUpdater;
+use JimLind\Pie7o\Tweeter;
+use JimLind\Pie7o\TweetFactory;
+
 /**
  * Configure all the things
  */
@@ -11,24 +19,24 @@ $settingList = [
     'consumerSecret'    => 'YOUR CONSUMER SECRET',
 ];
 
-$authorizationBuilder = new JimLind\Pie7o\AuthorizationBuilder($settingList);
+$authorizationBuilder = new AuthorizationBuilder($settingList);
 
-$statusUpdater = new JimLind\Pie7o\StatusUpdater($authorizationBuilder);
-$mediaUploader = new JimLind\Pie7o\MediaUploader($authorizationBuilder);
-$tweeter       = new JimLind\Pie7o\Tweeter($statusUpdater, $mediaUploader);
+$statusUpdater = new StatusUpdater($authorizationBuilder);
+$mediaUploader = new MediaUploader($authorizationBuilder);
+$tweeter       = new Tweeter($statusUpdater, $mediaUploader);
 
 /**
  * Create a Tweet object the fun way
  */
 $messageHandle = fopen('php://temp', 'r+');
-$messageStream = new GuzzleHttp\Psr7\Stream($messageHandle);
+$messageStream = new Stream($messageHandle);
 $messageStream->write('This is a pictures of cats.');
 $messageStream->rewind();
 
 $mediaHandle = fopen('./cat.jpg', 'r');
-$mediaStream = new GuzzleHttp\Psr7\Stream($mediaHandle);
+$mediaStream = new Stream($mediaHandle);
 
-$funTweet = (new JimLind\Pie7o\Tweet)
+$funTweet = (new Tweet)
     ->withMessage($messageStream)
     ->withMedia($mediaStream);
 
@@ -38,7 +46,7 @@ $funTweet = (new JimLind\Pie7o\Tweet)
 try {
     $tweeter->tweet($funTweet);
     echo 'The fun Tweet was successful.'.PHP_EOL;
-} catch (JimLind\Pie7o\Exception $exception) {
+} catch (Pie7oException $exception) {
     echo 'The fun Tweet has failed.'.PHP_EOL;
     echo 'Exception thrown: `'.$exception->getMessage().'`'.PHP_EOL;
 }
@@ -48,7 +56,7 @@ try {
  */
 $message    = 'This is the same picture of cats again.';
 $media      = './cat.jpg';
-$cheatTweet = JimLind\Pie7o\TweetFactory::buildTweet($message, $media);
+$cheatTweet = TweetFactory::buildTweet($message, $media);
 
 /**
  * Tweet and check results
@@ -56,7 +64,7 @@ $cheatTweet = JimLind\Pie7o\TweetFactory::buildTweet($message, $media);
 try {
     $tweeter->tweet($cheatTweet);
     echo 'The cheat Tweet was successful.'.PHP_EOL;
-} catch (JimLind\Pie7o\Pie7oException $exception) {
+} catch (Pie7oException $exception) {
     echo 'The cheat Tweet has failed.'.PHP_EOL;
     echo 'Exception thrown: `'.$exception->getMessage().'`'.PHP_EOL;
 }
