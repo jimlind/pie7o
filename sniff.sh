e8d39c695edc58f4aec8bd96e371774066878472
Jim Lind <spoon.vw@gmail.com>
2015-09-22 16:01:19 +0000

    Sniff the tests as well.

--- sniff.sh
+++ sniff.sh
@@ -1,2 +1,5 @@
 vendor/bin/phpcs src --standard=vendor/escapestudios/symfony2-coding-standard/Symfony2/ruleset.xml -p --colors
 vendor/bin/phpcs src --standard=PSR2 -p --colors
+
+vendor/bin/phpcs tests --standard=vendor/escapestudios/symfony2-coding-standard/Symfony2/ruleset.xml -p --colors
+vendor/bin/phpcs tests --standard=PSR2 -p --colors
